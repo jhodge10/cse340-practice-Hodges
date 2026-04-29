899bd005b47c603181c2252797572454dced1a4b
Jerry Hodges <[jhodge10]@users.noreply.github.com>
2026-04-28 18:10:35 -0600

    w02 unit1.2 routing

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 import express from 'express';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 const name = process.env.NAME; // <-- NEW
 
+/**
+ * Routes
+ */
 app.get('/', (req, res) => {
-    res.send(`Hello, ${name}!`); // <-- UPDATED
+    res.sendFile(path.join(__dirname, 'src/views/home.html'));
 });
 
-const PORT = 3000;
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/about.html'));
+});
+
+app.get('/products', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/products.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
