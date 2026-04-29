f48ed9ad0dd87cb3979ba5ee09951cd79a327819
Jerry Hodges <[jhodge10]@users.noreply.github.com>
2026-04-28 19:03:52 -0600

    w02 unit1.2 layouts and .ejs

--- server.js
+++ server.js
@@ -10,23 +10,33 @@ const app = express();
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
+
 const name = process.env.NAME; // <-- NEW
 
 /**
  * Routes
  */
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/home.html'));
+    const title = 'Welcome Home';
+    res.render('home', { title });
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/about.html'));
+    const title = 'About Me';
+    res.render('about', { title });
 });
 
 app.get('/products', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/products.html'));
+    const title = 'Our Products';
+    res.render('products', { title });
 });
 
+const NODE_ENV = process.env.NODE_ENV || 'production';
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
